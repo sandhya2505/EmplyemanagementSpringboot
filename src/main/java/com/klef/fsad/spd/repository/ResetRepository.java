de72379e683d643d943992c5a3c59130e3100fe7
Sandhya Nayak <107762980+sandhya2505@users.noreply.github.com>
2025-08-28 18:52:20 +0530

    Updated ResetRepository

--- src/main/java/com/klef/fsad/spd/repository/ResetRepository.java
+++ src/main/java/com/klef/fsad/spd/repository/ResetRepository.java
@@ -1,6 +1,9 @@
 package com.klef.fsad.spd.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.klef.fsad.spd.model.ResetToken;
@@ -8,6 +11,11 @@ import com.klef.fsad.spd.model.ResetToken;
 @Repository
 public interface ResetRepository extends JpaRepository<ResetToken,Long> {
 
+	@Query("select r from ResetToken r where r.token=?1 ")
+	public ResetToken FindByToken(String token);
 	
+	public Optional<ResetToken> findByToken(String token);
+	public Optional<ResetToken>findByEmail(String email);
+	public void deleteByToken(String token);
 	
 }
